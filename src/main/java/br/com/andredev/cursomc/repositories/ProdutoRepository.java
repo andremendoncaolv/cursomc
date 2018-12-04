a2171aae9a2a24913f4976298c5adc096eb438e5
Andre Mendonca <andremendoncaolv@gmail.com>
2018-12-04 09:16:21 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/br/com/andredev/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.andredev.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.andredev.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
